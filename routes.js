f75500bcf998324ecc6e4399844bd735941e0cc8
jesusGGZ <j.gr.zurita@gmail.com>
2021-02-27 09:48:01 -0600

    validaciones con Joi

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 'use strict'
+const joi = require('joi');
 const site = require('./public/controllers/site');
 const user = require('./public/controllers/user');
 
@@ -14,6 +15,15 @@ const user = require('./public/controllers/user');
     },
     {
         method: 'POST',
+        options: {
+            validate: {
+                payload: {
+                    name: Joi.string.required().min(3),
+                    email: Joi.string().email().required(),
+                    password: Joi.string().required().min(6)
+                }
+            }
+        },
         path: '/create-user',
         handler: user.createUser
     },
