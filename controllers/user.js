e6d8f516f7d96069bfe3034987f349f1b081a16f
jesusGGZ <j.gr.zurita@gmail.com>
2021-03-01 11:50:10 -0600

    Controlar el error 404 en inert y el error de vali

--- controllers/user.js
+++ controllers/user.js
@@ -49,7 +49,15 @@ async function validateUser (req, h){
 }
 
 function failValidation(req, h, err) {
-    Boom.badRequest('Falló la validación', req.payload)
+    const templates = {
+        '/create-user': 'register',
+        '/validate-user': 'login'
+    }
+
+    return h.view(templates[req.path], {
+        title: 'Error de validación',
+        error: 'Por favor complete los campos requeridos'
+    }).code(400).takeover()
 }
 
 module.exports = {
